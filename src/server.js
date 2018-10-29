e15da259ddc50f1208d660f56a91d0a68cfd182d
sunzongzheng <sunzongzhengwy@163.com>
2018-10-29 22:43:31 +0800

    修正Server.params && 增加leancloud部署配置

--- src/server.js
+++ src/server.js
@@ -1,38 +1,3 @@
-import express from 'express'
-import apicache from 'apicache'
-import musicApi from './app'
-
-const app = express()
-
-app.use(apicache.middleware('720 minutes'))
-
-app.get('/', async function (req, res) {
-    const method = req.query.method
-    const vendor = req.query.vendor
-    const params = req.query.params
-    const restParams = req.query.restParams
-
-    if (!method) {
-        res.status(400).send({
-            error: '参数错误'
-        })
-        return
-    }
-    let data
-    if (vendor) {
-        if (restParams) {
-            data = await musicApi[vendor][method](...restParams)
-        } else {
-            data = await musicApi[vendor][method](params)
-        }
-    } else {
-        if (restParams) {
-            data = await musicApi[method](...restParams)
-        } else {
-            data = await musicApi[method](params)
-        }
-    }
-    res.send(data)
-})
+import app from './express-app'
 
 app.listen(process.env.PORT || 3000)
